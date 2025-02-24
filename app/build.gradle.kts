e29d023898f0e06cf37fa5c54c26c2ab3b3ca977
Perniabeya <abeyabusiness@gmail.com>
2025-02-24 12:13:21 +0100

    Empty project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.calculadora_imc"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.calculadora_imc"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/calculadora_imc/MainActivity.kt
+++ app/src/main/java/com/example/calculadora_imc/MainActivity.kt
@@ -1,20 +1,54 @@
 package com.example.calculadora_imc
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var heightEditText: EditText
+    lateinit var weightEditText: EditText
+    lateinit var calculateButton: Button
+    lateinit var resultTextView: TextView
+    lateinit var descriptionTextView: TextView
+    lateinit var weightTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        heightEditText = findViewById(R.id.heightEditText)
+        weightEditText = findViewById(R.id.weightEditText)
+        calculateButton = findViewById(R.id.calculateButton)
+        resultTextView = findViewById(R.id.resultTextView)
+        descriptionTextView = findViewById(R.id.descriptionTextView)
+
+        calculateButton.setOnClickListener({
+            val height = heightEditText.text.toString().toFloat()
+            val weight = weightEditText.text.toString().toFloat()
+            val result = weight / (height / 100).pow(2)
+
+            resultTextView.text = "$result"
+
+            descriptionTextView.text = "Con una altura de $height cm y un peso de $weight cm tu masa corporal es de:"
+
+            heightEditText.setText("")
+            weightEditText.setText("")
+        })
     }
 }
